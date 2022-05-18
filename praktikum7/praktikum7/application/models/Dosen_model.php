3e6f31d6ff5dff5bb14a15b7d62643eb92823bd6
Allqis <93020748+Allqis@users.noreply.github.com>
2022-05-18 22:59:12 +0700

    Delete praktikum7/praktikum7 directory

--- praktikum7/praktikum7/application/models/Dosen_model.php
+++
@@ -1,7 +0,0 @@
-<?php
-class Dosen_model extends CI_Model {
-    // Buat Property atau Variabel
-    public $nidn;
-    public $pendidikan;
-}
-?>
--- praktikum7/praktikum7/application/models/Matakuliah_model.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Matakuliah_model extends CI_Model {
-    // Buat Property atau Variabel
-    public $nama;
-    public $sks;
-    public $kode;
-}
-?>
